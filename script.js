dbc042755f3695f16c28dc35b38cb7f493e14ada
itsjezi <159943357+itsjezi@users.noreply.github.com>
2024-02-14 02:01:20 +0800

    try

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('.container').mouseenter(function () {
+        $('.card').stop().animate({
+            top: '-90px'
+        }, 'slow');
+    }).mouseleave(function () {
+        $('.card').stop().animate({
+            top: 0
+        }, 'slow');
+    });
+});
+
+function openModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+  }
+  
+  function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+  }
